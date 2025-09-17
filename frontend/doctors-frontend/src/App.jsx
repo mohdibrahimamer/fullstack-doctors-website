0cb309dcff831689c1c7a7fc82ed41dad0745572
mohd ibrahim amer <mohdibrahimamer9@gmail.com>
2025-09-17 15:30:22 +0530

    started frontend functionalities for admin panel and users panel

---
+++ frontend/doctors-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-3xl">hello you will win over the world</p>
+    </>
+  );
+}
+
+export default App;
